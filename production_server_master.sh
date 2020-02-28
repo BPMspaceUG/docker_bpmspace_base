dc01e653ac4b400a52128b4b2461a8fe8e1ed550
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-02-28 16:12:22 +0100

    Update production_server_master.sh

--- production_server_master.sh
+++ production_server_master.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-touch /tmp/init_1.log
 chown -R www-data:www-data /var/www/
 find /var/www/ -type f -exec chmod 660 {} \;
 find /var/www/ -type d -exec chmod 770 {} \;
-touch /tmp/init_2.log
